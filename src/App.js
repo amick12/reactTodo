e815c057a6ff8e245906694097c4f1951cc537be
amick12 <amckee26@gmail.com>
2020-03-22 16:23:24 -0400

    begin input and method

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import Input from './Input';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello from App.js</h1>
-      <Input />
+    <div className='container d-flex justify-content-center'>
+      <div className='row'>
+        <div className='col-6 offset-4 text-center'>
+          <h1>Hello from App.js Container</h1>
+          <Input />
+        </div>
+      </div>
     </div>
   );
 }
--- src/Input.js
+++ src/Input.js
@@ -1,15 +1,37 @@
 import React from 'react';
 
-// input will require state -> use class
-
 class Input extends React.Component {
   constructor(props) {
     super(props);
+    // initialize state
+    this.state = {
+      newTodo: '',
+      todoList: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(e) {
+    // console.log('change handled');
+    this.setState({
+      newTodo: e.target.value
+    });
+  }
+
   render() {
+    // console.log(this.state.newTodo);
     return (
-      <div>
-        <p>y0, it's y0 Input, Drew!</p>
+      <div className='row'>
+        <input
+          type='text'
+          placeholder='yuz haz nu todo?'
+          className='text-center'
+          //set value to state of newTodo
+          value={this.state.newTodo}
+          //onChange attr execute handleChange fx
+          onChange={this.handleChange}
+        ></input>
       </div>
     );
   }
