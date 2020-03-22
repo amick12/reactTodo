e63217a7c37e6fa9202459b790b0e66b9ad265a3
amick12 <amckee26@gmail.com>
2020-03-22 15:50:39 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Input from './Input';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello from App.js</h1>
+      <Input />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Input.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// input will require state -> use class
+
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <p>y0, it's y0 Input, Drew!</p>
+      </div>
+    );
+  }
+}
+
+export default Input;
